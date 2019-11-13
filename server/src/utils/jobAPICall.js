27710480394900ac7633a7c6268ef50fe4b1c8bf
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-13 16:04:08 +0000

    tidy console logs

--- server/src/utils/jobAPICall.js
+++ server/src/utils/jobAPICall.js
@@ -1,23 +1,21 @@
 const axios = require("axios");
 
 const jobAPICall = async ({ location, description, full_time }) => {
-  console.log("IVE BEEN CALLED");
-  const locationParam = location ? `location=${location}` : "";
-  const descriptionParam = description
-    ? `${locationParam ? "&" : ""}description=${description}`
-    : "";
-  const fullTimeParam = full_time
-    ? `${locationParam ? "&" : ""}full_time=true`
-    : "";
+    const locationParam = location ? `location=${location}` : "";
+    const descriptionParam = description
+        ? `${locationParam ? "&" : ""}description=${description}`
+        : "";
+    const fullTimeParam = full_time
+        ? `${locationParam ? "&" : ""}full_time=true`
+        : "";
 
-  const url = `https://jobs.github.com/positions.json?${locationParam}${descriptionParam}${fullTimeParam}`;
+    const url = `https://jobs.github.com/positions.json?${locationParam}${descriptionParam}${fullTimeParam}`;
 
-  console.log(url, "URL");
-  let res = await axios.get(url);
+    let res = await axios.get(url);
 
-  let jobData = res.data;
+    let jobData = res.data;
 
-  return jobData;
+    return jobData;
 };
 
 module.exports = jobAPICall;
