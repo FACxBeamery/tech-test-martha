36ca36139f67a4b620c30b20bfcd3cf675a3d875
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-14 16:52:04 +0000

    use nock to mock external api

--- server/src/utils/jobAPICall.js
+++ server/src/utils/jobAPICall.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const jobAPICall = async ({ location, description, full_time }) => {
-    const locationParam = location ? `location=${location}` : "";
+    const locationParam = location ? `?location=${location}` : "";
     const descriptionParam = description
         ? `${locationParam ? "&" : ""}description=${description}`
         : "";
@@ -9,7 +9,7 @@ const jobAPICall = async ({ location, description, full_time }) => {
         ? `${locationParam ? "&" : ""}full_time=true`
         : "";
 
-    const url = `https://jobs.github.com/positions.json?${locationParam}${descriptionParam}${fullTimeParam}`;
+    const url = `https://jobs.github.com/positions.json${locationParam}${descriptionParam}${fullTimeParam}`;
 
     let res = await axios.get(url);
 
