1c9afd3c2c824125c283383d500cf510fab4aae5
marthaklambert <marthaklambert@marthas-mbp.home>
2019-11-14 08:56:59 +0000

    add jobslist to main app render

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,15 +1,36 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import LocationForm from "./LocationForm/LocationForm";
+import JobsList from "./JobsList/JobsList";
+import getJobs from "../../utils/getJobs";
+//ToDO is it bad to use context sometimes and other times not
+
+//TODO add back button to main page
 
 const Main = () => {
     const [location, setLocation] = useState(null);
+    const [jobsData, setJobsData] = useState(null);
+
+    useEffect(() => {
+        const getAPIJobs = async (location, setJobsData) => {
+            if (location) {
+                const apiData = await getJobs(location);
+                setJobsData(apiData);
+            }
+        };
+        getAPIJobs(location, setJobsData);
+    }, [location, setJobsData]);
+
     return (
         <>
-            {location ? (
-                <p>add jobs list</p>
+            {jobsData ? (
+                <JobsList
+                    location={location}
+                    jobsData={jobsData}
+                    setJobsData={setJobsData}
+                />
             ) : (
                 // <JobsList />
-                <LocationForm location={location} setLocation={setLocation} />
+                <LocationForm setLocation={setLocation} />
             )}
         </>
     );
