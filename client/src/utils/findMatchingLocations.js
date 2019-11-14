12985f8d964a15eb9461d05514b3c8868186811b
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-14 13:26:52 +0000

    write city filtering function

--- client/src/utils/findMatchingLocations.js
+++ client/src/utils/findMatchingLocations.js
@@ -1,5 +1,15 @@
-const findMatchingLocations = () => {
-    return ["london", "newyork"];
+import citiesList from "../data/citiesList";
+
+const removeSpacesLowercase = string => {
+    return string.replace(/^\s+|\s+$/g, "").toLowerCase();
+};
+const findMatchingLocations = inputString => {
+    const strippedLowercaseInput = removeSpacesLowercase(inputString);
+
+    const matchingCities = citiesList.filter(city =>
+        removeSpacesLowercase(city).startsWith(strippedLowercaseInput)
+    );
+    return matchingCities;
 };
 
 export default findMatchingLocations;
