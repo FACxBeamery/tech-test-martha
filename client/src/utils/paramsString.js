e22b26f7385a03f203c88d8a529c2cf7fc7cfae3
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-15 13:01:54 +0000

    use new params in get jobs

---
+++ client/src/utils/paramsString.js
@@ -0,0 +1,30 @@
+const paramsString = (location, description, full_time) => {
+    let queryString = "";
+    if (full_time === "All") {
+        full_time = null;
+    } else if (full_time === "Full Time") {
+        full_time = true;
+    } else if (full_time === "Full Time") {
+        full_time = false;
+    }
+    const fullTimeExists = full_time === true || full_time === false;
+    if (location) {
+        queryString += `?location=${location}`;
+        if (description) {
+            queryString += `&description=${description}`;
+        }
+        if (fullTimeExists) {
+            queryString += `&full_time=${full_time}`;
+        }
+    } else if (description) {
+        queryString += `?description=${description}`;
+        if (fullTimeExists) {
+            queryString += `&full_time=${full_time}`;
+        }
+    } else if (fullTimeExists) {
+        queryString += `?full_time=${full_time}`;
+    }
+    return queryString;
+};
+
+export default paramsString;
