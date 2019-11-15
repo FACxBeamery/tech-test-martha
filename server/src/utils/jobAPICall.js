bd897f8a02bdbafad5ec582a520d2da41ad75985
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-15 13:28:05 +0000

    edit full time param

--- server/src/utils/jobAPICall.js
+++ server/src/utils/jobAPICall.js
@@ -2,21 +2,29 @@ const axios = require("axios");
 
 const paramsString = (location, description, full_time) => {
     let queryString = "";
+    if (full_time === "All") {
+        full_time = null;
+    } else if (full_time === "Full Time") {
+        full_time = true;
+    } else if (full_time === "Full Time") {
+        full_time = false;
+    }
+    const fullTimeExists = full_time === true || full_time === false;
     if (location) {
         queryString += `?location=${location}`;
         if (description) {
             queryString += `&description=${description}`;
         }
-        if (full_time) {
-            queryString += `&full_time=true`;
+        if (fullTimeExists) {
+            queryString += `&full_time=${full_time}`;
         }
     } else if (description) {
         queryString += `?description=${description}`;
-        if (full_time) {
-            queryString += `&full_time=true`;
+        if (fullTimeExists) {
+            queryString += `&full_time=${full_time}`;
         }
-    } else if (full_time) {
-        queryString += `?full_time=true`;
+    } else if (fullTimeExists) {
+        queryString += `?full_time=${full_time}`;
     }
     return queryString;
 };
