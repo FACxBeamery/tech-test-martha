4a0288618d521e8ed07c7ab4a1abbac3365236ad
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-15 14:20:58 +0000

    add logic tests

---
+++ server/src/utils/paramString.test.js
@@ -0,0 +1,28 @@
+const paramsString = require("./paramsString");
+
+describe("params string should return correct list of params", () => {
+    test("it works with 3 params", () => {
+        const actual = paramsString("london", "a nice job", true);
+        const expected =
+            "?location=london&description=a nice job&full_time=true";
+        expect(actual).toEqual(expected);
+    });
+    test("it works with no location", () => {
+        const actual = paramsString(null, "a fun job", false);
+        const expected = "?description=a fun job&full_time=false";
+
+        expect(actual).toEqual(expected);
+    });
+    test("it works with only full time", () => {
+        const actual = paramsString(null, null, true);
+        const expected = "?full_time=true";
+
+        expect(actual).toEqual(expected);
+    });
+    test("it works with no params", () => {
+        const actual = paramsString(null, null, null);
+        const expected = "";
+
+        expect(actual).toEqual(expected);
+    });
+});
