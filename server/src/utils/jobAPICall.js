fd38e402cddcd069fc2292edbba7f9882eab6678
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-15 11:28:14 +0000

    make parameters in url more understandable

--- server/src/utils/jobAPICall.js
+++ server/src/utils/jobAPICall.js
@@ -1,15 +1,36 @@
 const axios = require("axios");
 
-const jobAPICall = async ({ location, description, full_time }) => {
-    const locationParam = location ? `?location=${location}` : "";
-    const descriptionParam = description
-        ? `${locationParam ? "&" : ""}description=${description}`
-        : "";
-    const fullTimeParam = full_time
-        ? `${locationParam ? "&" : ""}full_time=true`
-        : "";
+const paramsString = (location, description, full_time) => {
+    let queryString = "";
+    if (location) {
+        queryString += `?location=${location}`;
+        if (description) {
+            queryString += `&description=${description}`;
+        }
+        if (full_time) {
+            queryString += `&full_time=${full_time}`;
+        }
+    } else if (description) {
+        queryString += `?description=${description}`;
+        if (full_time) {
+            queryString += `&full_time=${full_time}`;
+        }
+    } else if (full_time) {
+        queryString += `?full_time=${full_time}`;
+    }
+    return queryString;
+};
 
-    const url = `https://jobs.github.com/positions.json${locationParam}${descriptionParam}${fullTimeParam}`;
+const replaceSpacesLowercase = str => {
+    const newString = str.replace(/ /g, "+").toLowerCase();
+    return newString;
+};
+const jobAPICall = async ({ location, description, full_time }) => {
+    const queryString = paramsString(location, description, full_time);
+
+    const urlQueryString = replaceSpacesLowercase(queryString);
+
+    const url = `https://jobs.github.com/positions.json${urlQueryString}`;
 
     let res = await axios.get(url);
 
